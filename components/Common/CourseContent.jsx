624b7ce211abc8a0fbc1cc50bf467b1f3b575805
mukulrajpoot262610 <mukulrajpoot262610@gmail.com>
2022-06-23 10:23:24 +0530

    refactor: course content

--- components/Common/CourseContent.jsx
+++ components/Common/CourseContent.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 export default function CourseContent() {
-  const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  const data = [1, 2];
+
+  const router = useRouter()
+  const path = router.pathname
+
   const elements = data.map((i, j) => {
     return (
       <div
         key={j}
-        className="p-4 overflow-hidden shadow bg-base-100 rounded-xl"
+        className="p-4 hover:border-red-400 hover:border overflow-hidden shadow bg-base-100 rounded-xl"
       >
         <figure className="overflow-hidden rounded-md aspect-h-1 aspect-w-2">
           <img
@@ -38,10 +44,27 @@ export default function CourseContent() {
     <section className="col-span-12 lg:col-span-9">
       <div className="w-full space-y-4 rounded-xl">
         <div className="space-y-2">
-          <h2 className="text-sm font-bold text-red-400">Enrolled Courses</h2>
-          <div className="grid grid-cols-1 gap-4 z-20 sm:grid-cols-2 lg:grid-cols-3">
-            {elements}
-          </div>
+          {
+            path === '/dashboard/courses' && <h2 className="text-sm font-bold text-red-400">Enrolled Courses</h2>
+          }
+
+          {
+            data.length === 0 ? <div className="w-full flex justify-center items-center flex-col h-full mt-8">
+              <img src="/no-data.svg" className="h-64" />
+              <h1 className="mt-8 font-bold text-2xl">No Courses Found...</h1>
+              {
+                path === '/dashboard/courses' && <>
+                  <p className="my-2">Try Enrolling in some courses.</p>
+                  <Link href="/explore">
+                    <button className="btn btn-ghost border border-red-300 mt-6 hover:bg-red-50">Explore Courses</button>
+                  </Link>
+                </>
+              }
+            </div> : <div className="grid grid-cols-1 gap-4 mt-4 z-20 sm:grid-cols-2 lg:grid-cols-3">
+              {elements}
+            </div>
+          }
+
         </div>
       </div>
     </section>
