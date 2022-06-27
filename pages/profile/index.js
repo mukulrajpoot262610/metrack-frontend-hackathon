ce276486891ae1685b4b254a6433dd6dd2f0b08d
mukulrajpoot262610 <mukulrajpoot262610@gmail.com>
2022-06-27 14:15:58 +0530

    refator: case error

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Profile from "../../components/Profile";
+import Profile from "../../components/Profile/index";
 
 export default function ProfilePage() {
     return (
