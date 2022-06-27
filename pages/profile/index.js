96cb5ae48c60bc1b289c9c83358b286bce826105
mukulrajpoot262610 <mukulrajpoot262610@gmail.com>
2022-06-27 14:32:27 +0530

    refator: profile

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,10 +1,9 @@
 import React from "react";
-import Profile from "../../components/Profile/index";
 
 export default function ProfilePage() {
     return (
         <div className="pb-10 mt-20">
-            <Profile />
+            <h1>Profile Page</h1>
         </div>
     );
 }
