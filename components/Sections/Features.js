eff8ae861f8a4e54368776cdfdab0f0ed7c25b7e
Amit Kumar <alsoamit@gmail.com>
2022-06-28 10:39:17 +0530

    refactor: home page

--- components/Sections/Features.js
+++ components/Sections/Features.js
@@ -2,45 +2,48 @@ import React from "react";
 
 const Features = () => {
   return (
-    <div className="relative  gap-4 w-full min-h-screen flex justify-center items-center flex-col">
-      <h1 className="text-3xl lg:text-5xl font-bold text-center">
+    <div className="relative flex flex-col items-center justify-center w-full min-h-screen gap-4">
+      <h1 className="text-3xl font-bold text-center lg:text-5xl">
         The Only “Platform” You Need To Learn
       </h1>
-      <p className="font-medium text-lg text-center">
+      <p className="text-lg font-medium text-center">
         Here are the possible scenarios to get the most out of this platform
       </p>
 
-      <div className="flex items-center flex-col lg:flex-row justify-center w-full mt-10">
-        <div className="w-full lg:w-1/2 flex flex-col gap-4 lg:p-10">
+      <div className="flex flex-col items-center justify-center w-full mt-10 lg:flex-row">
+        <div className="flex flex-col w-full gap-4 lg:w-1/2 lg:p-10">
           <div className="collapse collapse-arrow rounded-3xl">
             <input type="checkbox" className="peer" />
-            <div className="collapse-title text-xl font-medium peer-checked:bg-blue-100 ">
-              Specially Built for Generalists
+            <div className="text-xl font-medium collapse-title peer-checked:bg-blue-100 ">
+              Specially built for Developers
             </div>
-            <div className="collapse-content bg-white peer-checked:bg-red-100">
-              <p>tabIndex= attribute is necessary to make the div focusable</p>
+            <div className="pt-4 bg-white collapse-content peer-checked:bg-red-100">
+              <p>MeTrack provides hand-picked tutorials from youtube. </p>
             </div>
           </div>
           <div className="collapse collapse-arrow rounded-3xl">
             <input type="checkbox" className="peer" />
-            <div className="collapse-title text-xl font-medium peer-checked:bg-blue-100 ">
-              Specially Built for Generalists
+            <div className="text-xl font-medium collapse-title peer-checked:bg-blue-100 ">
+              Realtime Discussion Panel.
             </div>
-            <div className="collapse-content bg-white peer-checked:bg-red-100">
-              <p>tabIndex= attribute is necessary to make the div focusable</p>
+            <div className="pt-4 bg-white collapse-content peer-checked:bg-red-100">
+              <p>Discuss your doubts with others in real-time.</p>
             </div>
           </div>
           <div className="collapse collapse-arrow rounded-3xl">
             <input type="checkbox" className="peer" />
-            <div className="collapse-title text-xl font-medium peer-checked:bg-blue-100 ">
-              Specially Built for Generalists
+            <div className="text-xl font-medium collapse-title peer-checked:bg-blue-100 ">
+              Build in public, submit and view projects.
             </div>
-            <div className="collapse-content bg-white peer-checked:bg-red-100">
-              <p>tabIndex= attribute is necessary to make the div focusable</p>
+            <div className="pt-4 bg-white collapse-content peer-checked:bg-red-100">
+              <p>
+                Upload your project, get feedback from your peers, view projects
+                that others have built.
+              </p>
             </div>
           </div>
         </div>
-        <div className="w-full lg:w-1/2 p-10 flex justify-center items-center">
+        <div className="flex items-center justify-center w-full p-10 lg:w-1/2">
           <img
             src="https://fueler.io/images/fueler/main/fueler-internship-externship.webp"
             className="w-full"
