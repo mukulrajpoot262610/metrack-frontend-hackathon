2381fb9af0fe1c1f453f5e46e186844866116351
mukulrajpoot262610 <mukulrajpoot262610@gmail.com>
2022-06-28 17:44:04 +0530

    ADD: meta tags

--- components/Layout/Loader.js
+++ components/Layout/Loader.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import Head from 'next/head'
 
 const Loader = () => {
     return (
-        <main className='h-screen w-full bg-white flex justify-center items-center flex-col'>
-            <h1 className='animate-pulse text-4xl font-bold text-primary-blue'>Loading...</h1>
-        </main>
+        <>
+            <Head>
+                <title>MeTrack - Track your YouTube Learning</title>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
+                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+                <meta name="msapplication-TileColor" content="#00aba9" />
+                <meta name="theme-color" content="#ffffff" />
+            </Head>
+            <main className='h-screen w-full bg-white flex justify-center items-center flex-col'>
+                <h1 className='animate-pulse text-4xl font-bold text-primary-blue'>Loading...</h1>
+            </main>
+        </>
     )
 }
 
--- components/Layout/index.js
+++ components/Layout/index.js
@@ -15,7 +15,31 @@ const Layout = ({ children }) => {
 
   return (loading ? <Loader /> : <>
     <Head>
-      <title>SkillsTube</title>
+      <title>MeTrack - Track your YouTube Learning</title>
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="msapplication-TileColor" content="#00aba9" />
+      <meta name="theme-color" content="#ffffff" />
+      <meta name="description" content="MeTrack provides hand-picked YouTube courses, real-time discussions, sharing projects, and getting feedback on them. We believe in Build-in public and Proof of Work." />
+
+      <meta itemProp="name" content="MeTrack - Track your YouTube Learning" />
+      <meta itemProp="description" content="MeTrack provides hand-picked YouTube courses, real-time discussions, sharing projects, and getting feedback on them. We believe in Build-in public and Proof of Work." />
+      <meta itemProp="image" content="https://res.cloudinary.com/mukulrajpoot/image/upload/v1656417503/Untitled_design_2_v7ficz.png" />
+
+      <meta property="og:url" content="https://www.metrack.tech" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="MeTrack - Track your YouTube Learning" />
+      <meta property="og:description" content="MeTrack provides hand-picked YouTube courses, real-time discussions, sharing projects, and getting feedback on them. We believe in Build-in public and Proof of Work." />
+      <meta property="og:image" content="https://res.cloudinary.com/mukulrajpoot/image/upload/v1656417503/Untitled_design_2_v7ficz.png" />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="MeTrack - Track your YouTube Learning" />
+      <meta name="twitter:description" content="MeTrack provides hand-picked YouTube courses, real-time discussions, sharing projects, and getting feedback on them. We believe in Build-in public and Proof of Work." />
+      <meta name="twitter:image" content="https://res.cloudinary.com/mukulrajpoot/image/upload/v1656417503/Untitled_design_2_v7ficz.png" />
+
     </Head>
     {
       !path.includes("auth") && <Navbar />
